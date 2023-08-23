777818d6d69ac70ef619850308c946dddb53f051
fehervendel <fehervendel@gmail.com>
2023-08-23 14:54:13 +0200

    feature: add CompanyInventoryService singleton to program.cs

--- ToolManager/ToolManager/Program.cs
+++ ToolManager/ToolManager/Program.cs
@@ -1,13 +1,22 @@
+using ToolManager.Model;
+using ToolManager.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddSingleton(new CompanyInventoryService(new CompanyInventory())); //Is it ok?
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
