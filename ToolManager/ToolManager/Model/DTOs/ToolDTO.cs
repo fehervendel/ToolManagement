374ebdee8cc4c2a8b21405908e3ed3e1f4f9f106
fehervendel <fehervendel@gmail.com>
2023-08-23 14:55:06 +0200

    geature: add toolDTO

---
+++ ToolManager/ToolManager/Model/DTOs/ToolDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToolManager.Model.DTOs;
+
+public class ToolDTO
+{
+    [Required]
+    public string Type { get; set; }
+    
+    [Required]
+    public decimal Price { get; set; }
+}
