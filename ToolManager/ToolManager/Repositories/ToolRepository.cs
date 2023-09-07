035b18a0287668394c676f3993603fa78a5d9af1
fehervendel <fehervendel@gmail.com>
2023-09-07 09:40:54 +0200

    feature: add IToolRepository and implement ToolRepository

---
+++ ToolManager/ToolManager/Repositories/ToolRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToolManager.Context;
+using ToolManager.Model;
+
+namespace ToolManager.Repositories;
+
+public class ToolRepository : IToolRepository
+{
+    public async Task<IEnumerable<Tool>> GetAll()
+    {
+        using var dbContext = new ToolManagerContext();
+        return await dbContext.Tools.ToListAsync();
+    }
+    
+    public async Task<Tool?> GetById(int id)
+    {
+        using var dbContext = new ToolManagerContext();
+        return await dbContext.Tools.FirstOrDefaultAsync(e => e.Id == id);
+    }
+    
+    public async Task<IEnumerable<Tool?>> GetToolsByName(string name)
+    {
+        using var dbContext = new ToolManagerContext();
+        return await dbContext.Tools.Where(t => t.Type == name).ToListAsync();
+    }
+    
+    public async Task Add(Tool tool)
+    {
+        using var dbContext = new ToolManagerContext();
+        dbContext.Add(tool);
+        await dbContext.SaveChangesAsync();
+    }
+    
+    public async Task Delete(int id)
+    {
+        using var dbContext = new ToolManagerContext();
+        Tool tool = await dbContext.Tools.FirstOrDefaultAsync(t => t.Id == id);
+        dbContext.Remove(tool);
+        await dbContext.SaveChangesAsync();
+    }
+    
+    public async Task Update(Tool tool)
+    {
+        using var dbContext = new ToolManagerContext();
+        dbContext.Update(tool);
+        await dbContext.SaveChangesAsync();
+    }
+}
