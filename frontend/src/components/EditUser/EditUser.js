83e1c78fbef4692c66539f488f808a1ea5f42761
OrszaghLevente <orszagh.levi@gmail.com>
2023-10-03 15:09:09 +0200

    Add Tool feature + Css

--- frontend/src/components/EditUser/EditUser.js
+++ frontend/src/components/EditUser/EditUser.js
@@ -8,6 +8,7 @@ function EditUser() {
   const [user, setUser] = useState(null);
   const [newSalary, setNewSalary] = useState("");
   const [isEditing, setIsEditing] = useState(false);
+  const [isAddingTool, setIsAddingTool] = useState(false);
   const token = Cookies.get("userToken");
   const role = Cookies.get("userRole");
   
@@ -42,49 +43,84 @@ function EditUser() {
     setNewSalary(user?.salary || "");
   };
 
-  //TESZT
+  const fetchData = async () => {
+    const response = await fetch("/api/ToolManager/GetAllTools", {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+    const jsonData = await response.json();
+    setTools(jsonData);
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch("/api/ToolManager/GetAllTools", {
-        method: 'GET',
-        headers: {
-          'Authorization': `Bearer ${token}`
-        }
-      });
-      const jsonData = await response.json();
-      setTools(jsonData);
-    };
-    fetchData(); 
+      fetchData(); 
   }, [token]);
 
   if(role !== "Admin"){
     return <div>Only admins can see this page!</div>
   }
-  //ENDTESZT
+
 
   const handleSaveClick = async () => {
-    try {
-      // Send an API request to update the user's salary
-      const response = await fetch(`/api/ToolManager/UpdateUserSalary?id=${userId}&salary=${newSalary}`, { // Use the dynamic userId here
-        method: 'PUT',
-        headers: {
-            'Authorization': `Bearer ${token}`
-          }
-      });
+  try {
+    // Send an API request to update the user's salary
+    const response = await fetch(`/api/ToolManager/UpdateEmployeeSalary?id=${userId}&salary=${newSalary}`, {
+      method: 'PUT',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json', // Set the content type to JSON
+      },
+    });
 
+    if (response.status === 200) {
+      setIsEditing(false);
+      // Optionally, update the user's salary in the state
+      setUser({ ...user, salary: parseFloat(newSalary) });
+    } else {
+      // Handle error, e.g., display an error message
+      console.error(`Error updating user's salary: ${response.status}`);
+    }
+  } catch (error) {
+    // Handle network or other errors
+    console.error("Error updating user's salary:", error);
+  }
+};
+
+
+  const handleAddToUserClick = async (toolId) => {
+    try {
+      setIsAddingTool(true); // Disable the button while adding the tool
+  
+      const response = await fetch(
+        `/api/ToolManager/AddToolToEmployee?employeeId=${userId}&toolId=${toolId}`,
+        {
+          method: "PUT",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      fetchData();
       if (response.status === 200) {
-        setIsEditing(false);
-        // Optionally, update the user's salary in the state
-        setUser({ ...user, salary: parseFloat(newSalary) });
+        setIsAddingTool(false); // Re-enable the button after successful addition
+        // Optionally, update the user's tools or perform any necessary actions
+        console.log(`Tool with ID ${toolId} added to user with ID ${userId}`);
+        fetchData();
       } else {
+        setIsAddingTool(false); // Re-enable the button on error
         // Handle error, e.g., display an error message
-        console.error(`Error updating user's salary: ${response.status}`);
+        console.error(`Error adding tool to user: ${response.status}`);
       }
     } catch (error) {
+      setIsAddingTool(false); // Re-enable the button on error
       // Handle network or other errors
-      console.error("Error updating user's salary:", error);
+      console.error("Error adding tool to user:", error);
     }
+  
   };
+  
 
   if (!user) {
     return <div>Loading user details...</div>;
@@ -92,29 +128,33 @@ function EditUser() {
 
   return (
     <div>
-      <h2>Edit User</h2>
-      <div>
-        <p>User ID: {user.id}</p>
-        <p>Name: {user.name}</p>
-        <p>Email: {user.emailAddress}</p>
-        {isEditing ? (
-          <div>
-            <label>New Salary:</label>
-            <input
-              type="text"
-              value={newSalary}
-              onChange={(e) => setNewSalary(e.target.value)}
-              className="NewSalaryInput"
-            />
-            <button onClick={handleSaveClick} className="EditButton">Save</button>
-          </div>
-        ) : (
-          <div>
-            <p>Current Salary: {user.salary}</p>
-            <button onClick={handleEditClick} className="EditButton">Edit Salary</button>
-          </div>
-        )}
-      </div>
+    <h2>Edit User</h2>
+    <div className="user-details-box">
+      <p>User ID: {user.id}</p>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.emailAddress}</p>
+      {isEditing ? (
+        <div>
+          <label>New Salary:</label>
+          <input
+            type="text"
+            value={newSalary}
+            onChange={(e) => setNewSalary(e.target.value)}
+            className="NewSalaryInput"
+          />
+          <button onClick={handleSaveClick} className="EditButton">
+            Save
+          </button>
+        </div>
+      ) : (
+        <div>
+          <p>Current Salary: {user.salary}</p>
+          <button onClick={handleEditClick} className="EditButton">
+            Edit Salary
+          </button>
+        </div>
+      )}
+    </div>
       <div>
         <table className="tables">
           <thead>
@@ -123,22 +163,43 @@ function EditUser() {
               <th>Type</th>
               <th>Price</th>
               <th>Owner</th>
-              <th></th>
+              <th>Add tool</th>
+              <th>Remove tool</th>
             </tr>
           </thead>
           <tbody>
-            {tools &&
-              tools.map((tool, index) => (
-                tool.currentOwnerEmployee === null ? (
-                  <tr key={index}>
-                    <td>{tool.id}</td>
-                    <td>{tool.type}</td>
-                    <td>{tool.price}</td>
-                    <td>No Current Owner</td>
-                    <td><button type="button" className="EditButton">Add to user</button></td>
-                  </tr>
-                ) : null
-              ))}
+          {tools &&
+            tools.map((tool, index) => (
+              tool.currentOwnerEmployeeId === null || tool.currentOwnerEmployeeId === parseInt(userId) ? (
+                <tr key={index}>
+                  <td>{tool.id}</td>
+                  <td>{tool.type}</td>
+                  <td>{tool.price}</td>
+                  <td>{tool.currentOwnerEmployeeId === null ? "No current owner" : user.name}</td>
+                  <td>
+                    <button
+                      type="button"
+                      className="EditButton"
+                      onClick={() => handleAddToUserClick(Number(tool.id))}
+                      disabled={isAddingTool}
+                    >
+                      {isAddingTool ? "Adding..." : "Add to user"}
+                    </button>
+                  </td>
+                  <td>
+                    {tool.currentOwnerEmployeeId === parseInt(userId) && (
+                      <button
+                        type="button"
+                        className="RemoveButton"
+                        onClick={() => handleAddToUserClick(Number(tool.id))}
+                      >
+                        Remove tool
+                      </button>
+                    )}
+                  </td>
+                </tr>
+              ) : null
+            ))}
           </tbody>
         </table>
       </div>
