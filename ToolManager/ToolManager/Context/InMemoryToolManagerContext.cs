5088fdcc34c971b4d1236c9cef3587ae70fc4a22
fehervendel <fehervendel@gmail.com>
2023-10-04 11:25:57 +0200

    feature: add InMemoryDatabase

---
+++ ToolManager/ToolManager/Context/InMemoryToolManagerContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using ToolManager.Model;
+
+namespace ToolManager.Context;
+
+public class InMemoryToolManagerContext : DbContext
+{
+    public DbSet<Employee> Employees { get; set; }
+    
+    public DbSet<Tool> Tools { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase("TestDatabase");
+    }
+}
--- ToolManager/ToolManagerIntegrationTest/ToolManagerIntegrationTest.cs
+++ ToolManager/ToolManagerIntegrationTest/ToolManagerIntegrationTest.cs
@@ -1,9 +1,16 @@
 using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ToolManager.Context;
+using ToolManager.Contracts;
+using ToolManager.Controllers;
 using ToolManager.Model;
+using ToolManager.Repositories;
 using ToolManager.Services.Authentication;
 
 namespace ToolManagerIntegrationTest;
@@ -12,12 +19,14 @@ namespace ToolManagerIntegrationTest;
 public class ToolManagerIntegrationTest : WebApplicationFactory<Program>
 {
     private HttpClient _client;
-    private IAuthService _authService;
-    
+    private InMemoryToolManagerContext _context;
+
 
     [SetUp]
     public void Setup()
     {
+        _context = new InMemoryToolManagerContext();
+
         DotNetEnv.Env.Load();
         string connectionString = DotNetEnv.Env.GetString("CONNECTION_STRING");
         Environment.SetEnvironmentVariable("CONNECTION_STRING", connectionString);
@@ -25,32 +34,67 @@ public class ToolManagerIntegrationTest : WebApplicationFactory<Program>
         {
             PropertyNameCaseInsensitive = true
         };
-        
+
         _client = CreateClient();
         AuthRequest authRequest = new AuthRequest("admin@admin.com", "admin123");
         string jsonString = JsonSerializer.Serialize(authRequest);
         StringContent jsonStringContent = new StringContent(jsonString);
         jsonStringContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
         var response = _client.PostAsync("Auth/Login", jsonStringContent).Result;
         var content = response.Content.ReadAsStringAsync().Result;
         var desContent = JsonSerializer.Deserialize<AuthResponse>(content, options);
         var token = desContent.Token;
-       _client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+        _client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
     }
 
     [OneTimeTearDown]
     public void TearDown()
     {
         _client.Dispose();
+        _context.Dispose();
     }
-    
+
     [Test]
     public async Task GetAllEmployees_ShouldReturnOk()
     {
         var response = await _client.GetAsync("/api/ToolManager/GetAllEmployees");
 
         response.EnsureSuccessStatusCode();
-        
-        Assert.AreEqual(System.Net.HttpStatusCode.OK, response.StatusCode);
+
+        var content = await response.Content.ReadAsStringAsync();
+        var employees = JsonSerializer.Deserialize<List<Employee>>(content);
+
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+        Assert.IsNotNull(employees);
+        Assert.IsTrue(employees.Count > 0);
+    }
+
+    [Test]
+    public async Task AddEmployee_ShouldReturnOk()
+    {
+        RegistrationRequest requestContent = new RegistrationRequest("example@example.com", "JohnDoe", "password123");
+
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        await using InMemoryToolManagerContext context = new InMemoryToolManagerContext();
+        Employee addedEmployee = new Employee
+        {
+            Name = requestContent.Username,
+            EmailAddress = requestContent.Email
+        };
+
+        context.Employees.Add(addedEmployee);
+        await context.SaveChangesAsync();
+
+        var retrievedEmployee = await context.Employees.FindAsync(addedEmployee.Id);
+
+        Assert.IsNotNull(retrievedEmployee);
+        Assert.That(retrievedEmployee?.Name, Is.EqualTo(requestContent.Username));
+        Assert.That(retrievedEmployee?.EmailAddress, Is.EqualTo(requestContent.Email));
     }
 }
