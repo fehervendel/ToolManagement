7034538a3cde9ddb417dbc0a8b1df05d35ba38fb
fehervendel <fehervendel@gmail.com>
2023-10-16 14:08:49 +0200

    refactor: change api url in MyTools.js

--- frontend/src/components/myTools/MyTools.js
+++ frontend/src/components/myTools/MyTools.js
@@ -10,7 +10,7 @@ function MyTools() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch("https://toolmanagerserver.onrender.com/api/ToolManager/GetAllTools", {
+      const response = await fetch("https://localhost:7173/api/ToolManager/api/ToolManager/GetAllTools", {
         method: 'GET',
         headers: {
           'Authorization': `Bearer ${token}`
