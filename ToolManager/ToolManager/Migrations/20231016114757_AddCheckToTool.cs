d31673981dd2ad13c1c7410ef56416fa729923b4
fehervendel <fehervendel@gmail.com>
2023-10-16 14:05:57 +0200

    feature: create new migration for "Check"

---
+++ ToolManager/ToolManager/Migrations/20231016114757_AddCheckToTool.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToolManager.Migrations
+{
+    public partial class AddCheckToTool : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Check",
+                table: "Tools",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Check",
+                table: "Tools");
+        }
+    }
+}
